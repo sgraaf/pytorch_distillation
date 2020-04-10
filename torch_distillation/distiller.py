d5df88a45f169c7b1875ea8c399f637c2a84df63
sgraaf <sgraaf@users.noreply.github.com>
2020-04-10 15:21:23 +0200

    Implemented gradient accumulation and clipping

--- torch_distillation/distiller.py
+++ torch_distillation/distiller.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 from torch.nn.modules.loss import _Loss as Loss
 from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.utils import clip_grad_norm_
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import _LRScheduler as Scheduler
 from torch.utils.data import DataLoader
@@ -25,9 +26,12 @@ class _Distiller(object):
         loss_fn: Loss,
         optimizer: Optimizer,
         num_epochs: int,
+        num_gradient_accumulation_steps: Optional[int] = 1,
+        max_gradient_norm: Optional[float] = None,
         scheduler: Optional[Scheduler] = None,
         use_tqdm: Optional[bool] = True,
         logger: Optional[Logger] = None,
+
     ) -> None:
         self.student = student
         self.teacher = teacher
@@ -35,6 +39,8 @@ class _Distiller(object):
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.num_epochs = num_epochs
+        self.num_gradient_accumulation_steps = num_gradient_accumulation_steps
+        self.max_gradient_norm = max_gradient_norm
         self.scheduler = scheduler
         self.use_tqdm = use_tqdm
         self.logger = logger
@@ -56,8 +62,11 @@ class Distiller(_Distiller):
         loss_fn: The loss function.
         optimizer: The optimizer.
         num_epochs: The number of distillation epochs (iterations over the complete dataset).
-        scheduler: The learning rate scheduler. (default: None)
         use_cuda: Whether to use cuda or not. (default: False)
+        num_gradient_accumulation_steps: The number of steps in which the gradient are accumulated (default: 1)
+        max_gradient_norm: The maximum gradient norm (for clipping). (default: None)
+        scheduler: The learning rate scheduler. (default: None)
+        use_tqdm: Whether to use tqdm (progress bar) or not. (default: True)
         logger: The logger. (default: None)
     """
 
@@ -69,9 +78,11 @@ class Distiller(_Distiller):
         loss_fn: Loss,
         optimizer: Optimizer,
         num_epochs: int,
+        use_cuda: Optional[bool] = False,
+        num_gradient_accumulation_steps: Optional[int] = 1,
+        max_gradient_norm: Optional[float] = None,
         scheduler: Optional[Scheduler] = None,
         use_tqdm: Optional[bool] = True,
-        use_cuda: Optional[bool] = False,
         logger: Optional[Logger] = None,
     ) -> None:
         super(Distiller, self).__init__(
@@ -81,6 +92,8 @@ class Distiller(_Distiller):
             loss_fn,
             optimizer,
             num_epochs,
+            num_gradient_accumulation_steps,
+            max_gradient_norm,
             scheduler,
             logger,
         )
@@ -109,9 +122,6 @@ class Distiller(_Distiller):
                 )
             
             for step, batch in enumerate(self.dataloader):
-                # clear all gradients
-                self.optimizer.zero_grad()
-
                 # unpack batch
                 input, target = batch
 
@@ -126,13 +136,24 @@ class Distiller(_Distiller):
                 # compute the loss
                 loss = self.loss_fn(student_logits, teacher_logits)
 
+                # rescale the loss
+                loss /= self.num_gradient_accumulation_steps
+
                 # backward pass
                 loss.backward()
 
-                # update the parameters
-                self.optimizer.step()
-                if self.scheduler is not None:
-                    self.scheduler.step()
+                if step % self.num_gradient_accumulation_steps == 0:
+                    # clip the gradient
+                    if self.max_gradient_norm is not None:
+                        clip_grad_norm_(self.student.parameters(), self.max_gradient_norm)
+                    
+                    # update the parameters
+                    self.optimizer.step()
+                    if self.scheduler is not None:
+                        self.scheduler.step()
+
+                    # clear all gradients
+                    self.optimizer.zero_grad()
 
                 # update the progress bar
                 if self.use_tqdm:
@@ -156,7 +177,11 @@ class DistributedDistiller(_Distiller):
         optimizer: The optimizer.
         num_epochs: The number of distillation epochs (iterations over the complete dataset).
         local_rank: The local rank of the process.
+        is_master: Whether the local process is the master process. (default: True)
+        num_gradient_accumulation_steps: The number of steps in which the gradient are accumulated (default: 1)
+        max_gradient_norm: The maximum gradient norm (for clipping). (default: None)
         scheduler: The learning rate scheduler. (default: None)
+        use_tqdm: Whether to use tqdm (progress bar) or not. (default: True)
         logger: The logger. (default: None)
     """
 
@@ -169,6 +194,9 @@ class DistributedDistiller(_Distiller):
         optimizer: Optimizer,
         num_epochs: int,
         local_rank: int,
+        is_master: Optional[bool] = True,
+        num_gradient_accumulation_steps: Optional[int] = 1,
+        max_gradient_norm: Optional[float] = None,
         scheduler: Optional[Scheduler] = None,
         use_tqdm: Optional[bool] = True,
         logger: Optional[Logger] = None,
@@ -180,10 +208,13 @@ class DistributedDistiller(_Distiller):
             loss_fn,
             optimizer,
             num_epochs,
+            num_gradient_accumulation_steps,
+            max_gradient_norm,
             scheduler,
             logger,
         )
         self.local_rank = local_rank
+        self.is_master = is_master
 
         # send models to GPU
         self.student = self.student.to(f'cuda:{self.local_rank}')
@@ -205,7 +236,7 @@ class DistributedDistiller(_Distiller):
             # synchronize all processes
             torch.distributed.barrier()
 
-            if self.logger is not None:
+            if self.is_master and self.logger is not None:
                 self.logger.info(f'Starting with epoch {epoch+1}/{self.num_epochs}')
 
             # initialize the progress bar
@@ -218,9 +249,6 @@ class DistributedDistiller(_Distiller):
                 )
             
             for step, batch in enumerate(self.dataloader):
-                # clear all gradients
-                self.optimizer.zero_grad()
-
                 # unpack batch
                 input, target = batch
 
@@ -235,14 +263,25 @@ class DistributedDistiller(_Distiller):
                 # compute the loss
                 loss = self.loss_fn(student_logits, teacher_logits)
                 loss = loss.mean()
+                
+                # rescale the loss
+                loss /= self.num_gradient_accumulation_steps
 
                 # backward pass
                 loss.backward()
 
-                # update the parameters
-                self.optimizer.step()
-                if self.scheduler is not None:
-                    self.scheduler.step()
+                if step % self.num_gradient_accumulation_steps == 0:
+                    # clip the gradient
+                    if self.max_gradient_norm is not None:
+                        clip_grad_norm_(self.student.parameters(), self.max_gradient_norm)
+
+                    # update the parameters
+                    self.optimizer.step()
+                    if self.scheduler is not None:
+                        self.scheduler.step()
+
+                    # clear all gradients
+                    self.optimizer.zero_grad()
 
                 # update the progress bar
                 if self.use_tqdm:
